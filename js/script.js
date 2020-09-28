aad2df9da571ad02829749811f82a601845c1aa7
Unknown <davideamabile81@gmail.com>
2020-09-28 15:50:44 +0200

    chiamata

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
+ // creare to do list
+ $.ajax(
+   {
+     "url":  "http://157.230.17.132:3003/todos",
+      "method": "GET",
+      "success": function (data) {
+     // invoco funzione per il template
+      console.log(data);
+      renderList(data);
 
+    },
+      "error": function () {
+       alert("E' avvenuto un errore. " );
+    }
+   }
+ );
+ // fine chiamata ajax
 });
+
+// creare funzione crearzione template
+function renderList(data){
+  var source = $("#list-template").html();
+  var template = Handlebars.compile(source);
+
+  for (i=0; i<=data.length; i++){
+    console.log(data[i].text);
+  }
+    // console.log(data[i].text);
+}
